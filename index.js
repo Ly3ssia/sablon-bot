5357edc95afa03504e86e41726ec7f258d286594
Ly3ssia <93944142+Ly3ssia@users.noreply.github.com>
2022-11-09 23:19:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], shards: "auto", partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember]});
+const config = require("./src/config.js");
+const { readdirSync } = require("fs")
+const moment = require("moment");
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+
+let token = config.token
+
+client.commands = new Collection()
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+const log = l => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${l}`) };
+
+//command-handler
+const commands = [];
+readdirSync('./src/commands').forEach(async file => {
+  const command = require(`./src/commands/${file}`);
+  commands.push(command.data.toJSON());
+  client.commands.set(command.data.name, command);
+})
+
+client.on("ready", async () => {
+
+        try {
+            await rest.put(
+                Routes.applicationCommands(client.user.id),
+                { body: commands },
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    log(`${client.user.username} Aktif Edildi!`);
+})
+
+//event-handler
+readdirSync('./src/events').forEach(async file => {
+	const event = require(`./src/events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+})
+//
+
+client.login(token)
